545324283b03612cc1d91a0872fe4dc2a82481a8
idanacumen <idan@acumen.io>
2021-07-10 21:05:46 +0300

    related commit using pr

--- src/queries/ps_samples_extraction.sql
+++ src/queries/ps_samples_extraction.sql
@@ -88,3 +88,118 @@ dataset_type = 'Test'
 #and
 #substr(commit, 10,1) in ('1', '2', '3', '4', '5', '6', '7','8', '9', 'a', 'c')
 ;
+
+# Extracts the data set for commit summarization
+
+drop table if exists general.related_train_sample_b1;
+
+create table
+general.related_train_sample_b1
+as
+select
+pair.*
+, pair_details.subject  as subject2
+, pair_details.message  as message2
+
+from
+(
+select
+ec1.repo_name
+, ec1.commit as commit1
+, max(ec2.commit) as commit2
+, max(ec1.author_name) as author_name
+, max(ec1.subject)  as subject1
+, max(ec1.message)  as message1
+from
+general.enhanced_commits as ec1
+join
+general.train_sample_b1 as d1
+on
+ec1.repo_name = d1.repo_name
+and
+ec1.commit = d1.commit
+join
+general.enhanced_commits as ec2
+on
+ec1.repo_name = ec2.repo_name
+and
+ec1.author_name = ec2.author_name
+and
+abs(TIMESTAMP_DIFF(ec1.commit_timestamp, ec2.commit_timestamp, day)) <= 7
+join
+general.plain_commits_dataset as d2
+on
+ec2.repo_name = d2.repo_name
+and
+ec2.commit = d2.commit
+group by
+ec1.repo_name
+, ec1.commit
+) as pair
+join
+general.enhanced_commits as pair_details
+on
+pair.repo_name = pair_details.repo_name
+and
+pair.commit2 = pair_details.commit
+
+;
+
+
+drop table if exists general.related_corrective_train_sample_b1;
+
+create table
+general.related_corrective_train_sample_b1
+as
+select
+pair.*
+, pair_details.subject  as subject2
+, pair_details.message  as message2
+
+from
+(
+select
+ec1.repo_name
+, ec1.commit as commit1
+, max(ec2.commit) as commit2
+, max(ec1.author_name) as author_name
+, max(ec1.subject)  as subject1
+, max(ec1.message)  as message1
+from
+general.enhanced_commits as ec1
+join
+general.train_sample_b1 as d1
+on
+ec1.repo_name = d1.repo_name
+and
+ec1.commit = d1.commit
+join
+general.enhanced_commits as ec2
+on
+ec1.repo_name = ec2.repo_name
+and
+ec1.author_name = ec2.author_name
+and
+abs(TIMESTAMP_DIFF(ec1.commit_timestamp, ec2.commit_timestamp, day)) <= 7
+join
+general.plain_commits_dataset as d2
+on
+ec2.repo_name = d2.repo_name
+and
+ec2.commit = d2.commit
+where
+ec1.is_corrective
+and
+ec2.is_corrective
+group by
+ec1.repo_name
+, ec1.commit
+) as pair
+join
+general.enhanced_commits as pair_details
+on
+pair.repo_name = pair_details.repo_name
+and
+pair.commit2 = pair_details.commit
+
+;
