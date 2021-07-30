3645743f9ce37a37d9c4c940b8ab623c323177d5
idanacumen <idan@acumen.io>
2021-07-30 18:53:39 +0300

    comsum_random_batch_12_july_2021

--- src/queries/commit_sampling_for_labeling.sql
+++ src/queries/commit_sampling_for_labeling.sql
@@ -1,16 +1,24 @@
 select
-repo_name
-, commit
-, subject
-, message_without_subject
+d.repo_name
+, d.commit
+, d.subject
+, d.message_without_subject
+, ec.parents > 1 as multiple_parents
 , '' as Is_Summary
 , '' as Is_Generic
+, '' as Category
 , '' as Comment
 , '' as Certain
 , '' as Labler
-, 'comsum_random_batch_9_july_2021' as Sampling
+, 'comsum_random_batch_12_july_2021' as Sampling
 from
-general.plain_commits_dataset
+general.plain_commits_dataset as d
+join
+general.enhanced_commits as ec
+on
+d.repo_name = ec.repo_name
+and
+d.commit = ec.commit
 order by
 rand()
 limit 5000
